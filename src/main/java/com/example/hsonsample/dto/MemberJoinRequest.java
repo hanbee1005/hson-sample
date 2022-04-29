8cdfa3362020468881b46500523fa26ebb776194
hanbee <sonhanbi1002@gmail.com>
2022-04-29 18:31:10 +0900

    validation 설정 추가

--- src/main/java/com/example/hsonsample/dto/MemberJoinRequest.java
+++ src/main/java/com/example/hsonsample/dto/MemberJoinRequest.java
@@ -1,9 +1,15 @@
 package com.example.hsonsample.dto;
 
+import javax.validation.constraints.NotEmpty;
+
 public class MemberJoinRequest {
+    @NotEmpty
     private String email;
+    @NotEmpty
     private String password;
+    @NotEmpty
     private String name;
+
     private String birthdate;
     private String phoneNumber;
 }
