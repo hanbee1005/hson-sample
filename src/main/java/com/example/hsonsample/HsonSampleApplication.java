64620c58e3dd4abb02cec6d0aece16f6c6959bc0
hanbee <sonhanbi1002@gmail.com>
2022-04-29 14:13:58 +0900

    init commit

---
+++ src/main/java/com/example/hsonsample/HsonSampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.hsonsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HsonSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HsonSampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/hsonsample/HsonSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hsonsample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HsonSampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
