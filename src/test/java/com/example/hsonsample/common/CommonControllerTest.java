dba34b2026455f3d307c180799d6541d83ae0796
hanbee <sonhanbi1002@gmail.com>
2022-09-28 16:45:06 +0900

    최종 결과 파일 지정해서 나올 수 있게 수정 및 인코딩 수정

--- src/test/java/com/example/hsonsample/common/CommonControllerTest.java
+++ src/test/java/com/example/hsonsample/common/CommonControllerTest.java
@@ -2,11 +2,14 @@ package com.example.hsonsample.common;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.restdocs.RestDocumentationContextProvider;
 import org.springframework.restdocs.RestDocumentationExtension;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.filter.CharacterEncodingFilter;
 
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
 
@@ -14,12 +17,15 @@ import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.docu
 public abstract class CommonControllerTest {
     protected static final String API_V1 = "/v1";
 
+    @Autowired
     protected MockMvc mockMvc;
 
     @BeforeEach
     public void setUp(WebApplicationContext webApplicationContext, RestDocumentationContextProvider restDocumentation) {
         this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
                 .apply(documentationConfiguration(restDocumentation))
+                .alwaysDo(MockMvcResultHandlers.print())
+                .addFilters(new CharacterEncodingFilter("UTF-8",true))
                 .build();
     }
 }
